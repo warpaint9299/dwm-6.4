e91a1fb00d4f951623149845f2e6690aeaef05fd
warpaint <warpaint9299@gmail.com>
2024-02-24 21:31:30 +0800

    update

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
-fcitx5 &
 feh --randomize --bg-fill ~/.wallpaper/*.png &
 picom --experimental-backends --config ~/.config/picom/picom.conf >> /dev/null 2>&1 & 
+nm-applet &
+fcitx5 &
+flameshot &
+xfce4-panel --disable-wm-check &
--- config.def.h
+++ config.def.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 8;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const char panel[][20]       = { "xfce4-panel", "Xfce4-panel" }; /* name & cls of panel win */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 0;       /* vertical padding of bar */
@@ -64,6 +65,9 @@ static const unsigned int alphas[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+/* Lockfile */
+static char lockfile[] = "/tmp/dwm.lock";
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -73,6 +77,7 @@ static const Rule rules[] = {
 	{ "alacritty",            NULL,       NULL,      0,               0,     	     0,		                -1 },
 	{ "firefox-esr",          NULL,       NULL,      2,       		  0,             0,                     -1 },
 	{ "Google-chrome",        NULL,       NULL,      2,       		  0,             0,                     -1 },
+	{  panel[1],              NULL,       NULL,      (1 << 9) - 1,    1,             0,                     -1 },
 };
 
 /* layout(s) */
