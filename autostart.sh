be263f7f9c0a4e0ef847585a4b156f022cb20a1f
warpaint <warpaint9299@gmail.com>
2024-02-24 22:28:32 +0800

    update

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+
+systemctl --user import-environment
 feh --randomize --bg-fill ~/.wallpaper/*.png &
 picom --experimental-backends --config ~/.config/picom/picom.conf >> /dev/null 2>&1 & 
-nm-applet &
 fcitx5 &
+nm-applet &
+blueman-applet &
 flameshot &
 xfce4-panel --disable-wm-check &
