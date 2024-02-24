b33f0fa6310fa061948ba3b633472ede6f021d33
warpaint <warpaint9299@gmail.com>
2024-02-25 00:21:33 +0800

    update

--- autostart.sh
+++ autostart.sh
@@ -11,7 +11,7 @@ done &
 
 (
 while true; do
-	picom --experimental-backends --config ~/.config/picom/picom.conf >> /dev/null 2>&1
+	[[ $(pgrep -x 'picom') ]] || picom --experimental-backends --config ~/.config/picom/picom.conf >> /dev/null 2>&1
 	sleep 3;
 done &
 )
