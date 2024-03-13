b28e8a801c88b4e6d1549dc6040aa6367885e069
warpaint <warpaint9299@gmail.com>
2024-03-14 00:09:15 +0800

    update

--- dwm.c
+++ dwm.c
@@ -970,23 +970,27 @@ drawbars(void)
 void
 enqueue(Client *c)
 {
-	Client *l;
-	for (l = c->mon->clients; l && l->next; l = l->next);
-	if (l) {
-		l->next = c;
-		c->next = NULL;
-	}
+    Client *l;
+    for (l = c->mon->clients; l && l->next; l = l->next)
+        ;
+    if (l)
+    {
+        l->next = c;
+        c->next = NULL;
+    }
 }
 
 void
 enqueuestack(Client *c)
 {
-	Client *l;
-	for (l = c->mon->stack; l && l->snext; l = l->snext);
-	if (l) {
-		l->snext = c;
-		c->snext = NULL;
-	}
+    Client *l;
+    for (l = c->mon->stack; l && l->snext; l = l->snext)
+        ;
+    if (l)
+    {
+        l->snext = c;
+        c->snext = NULL;
+    }
 }
 
 void
@@ -1757,33 +1761,37 @@ restack(Monitor *m)
 void
 rotatestack(const Arg *arg)
 {
-	Client *c = NULL, *f;
+    Client *c = NULL, *f;
 
-	if (!selmon->sel)
-		return;
-	f = selmon->sel;
-	if (arg->i > 0) {
-		for (c = nexttiled(selmon->clients); c && nexttiled(c->next); c = nexttiled(c->next));
-		if (c){
-			detach(c);
-			attach(c);
-			detachstack(c);
-			attachstack(c);
-		}
-	} else {
-		if ((c = nexttiled(selmon->clients))){
-			detach(c);
-			enqueue(c);
-			detachstack(c);
-			enqueuestack(c);
-		}
-	}
-	if (c){
-		arrange(selmon);
-		//unfocus(f, 1);
-		focus(f);
-		restack(selmon);
-	}
+    if (!selmon->sel)
+        return;
+    f = selmon->sel;
+    if (arg->i > 0)
+    {
+        for (c = nexttiled(selmon->clients); c && nexttiled(c->next); c = nexttiled(c->next))
+            ;
+        if (c)
+        {
+            detach(c);
+            attach(c);
+            detachstack(c);
+            attachstack(c);
+        }
+    }
+    else if ((c = nexttiled(selmon->clients)))
+    {
+        detach(c);
+        enqueue(c);
+        detachstack(c);
+        enqueuestack(c);
+    }
+    if (c)
+    {
+        arrange(selmon);
+        // unfocus(f, 1);
+        focus(f);
+        restack(selmon);
+    }
 }
 
 void
