081306e283c47180a32da08598fa5d4b6ea3acce
warpaint <warpaint9299@email.com>
2024-06-09 23:44:21 +0800

    update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -42,11 +42,16 @@ while true; do
 done &
 )
 
+(
+while true; do
+
+	[[ $(pgrep -x 'xautolock') ]] || xautolock -detectsleep -time 5 -locker 'i3lock -c 0e0e0e -n' >> /dev/null 2>&1 &
+	sleep 300;
+done &
+)
 blueman-applet &
 flameshot &
 fcitx5 &
 xfce4-panel --disable-wm-check &
 tmux has-session && exec st -e tmux attach || exec st -e tmux new -s Workspace01 &
-xautolock -detectsleep -time 5 -locker 'i3lock -c 0e0e0e -n' >> /dev/null 2>&1 &
 exec xset -b & # disable console bell volume
-
