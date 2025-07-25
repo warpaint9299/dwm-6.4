8c13eaa217701cf1a31094d06d2a247926b2dd81
warpaint <warpaint9299@email.com>
2025-07-25 23:26:31 +0800

    add wacom settings into autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -31,12 +31,19 @@ while true; do
 	sleep 5;
 done &
 )
-(
 
+(
 while true; do
 	[[ $(pgrep -x 'xautolock') ]] || xautolock -detectsleep -time 5 -locker 'i3lock -c 0e0e0e -n' >> /dev/null 2>&1 &
 	sleep 300;
 done &
+)
+
+# wacom settings
+(
+xsetwacom set "Wacom Intuos BT M Pen stylus" MapToOutput DisplayPort-0
+xsetwacom set "Wacom Intuos BT M Pad pad" Button 1 "key Ctrl z"
+xsetwacom set "Wacom Intuos BT M Pad pad" Button 2 "key Shift Ctrl o"
 )
 
 blueman-applet &
@@ -45,4 +52,6 @@ flameshot &
 xfce4-panel --disable-wm-check &
 sleep 0.3
 tmux has-session && exec kitty tmux attach || exec kitty tmux &
+
+
 exec xset -b & # disable console bell volume
